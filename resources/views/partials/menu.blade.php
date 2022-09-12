4cb280143ba9649451617d55052189ca0ed6e5e6
ayamout1 <105465622+ayamout1@users.noreply.github.com>
2022-09-12 11:49:58 -0500

    added links to reports

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -35,6 +35,37 @@
                         </p>
                     </a>
                 </li>
+
+                <li class="nav-item">
+                    <a href="{{ route("admin.mainfilter.reportprelude") }}" class="nav-link ">
+                        <i class="fa-fw nav-icon fas fa-server">
+
+                        </i>
+                        <p>
+                            Missing Prelude Numbers
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route("admin.mainfilter.reportvendornumbers") }}" class="nav-link ">
+                        <i class="fa-fw nav-icon fas fa-server">
+
+                        </i>
+                        <p>
+                            Missing Vendor Numbers
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route("admin.mainfilter.listvp") }}" class="nav-link ">
+                        <i class="fa-fw nav-icon fas fa-server">
+
+                        </i>
+                        <p>
+                            Listing Vendor and Prelude Numbers
+                        </p>
+                    </a>
+                </li>
                 @can('user_management_access')
                     <li class="nav-item has-treeview {{ request()->is("admin/permissions*") ? "menu-open" : "" }} {{ request()->is("admin/roles*") ? "menu-open" : "" }} {{ request()->is("admin/users*") ? "menu-open" : "" }} {{ request()->is("admin/audit-logs*") ? "menu-open" : "" }}">
                         <a class="nav-link nav-dropdown-toggle" href="#">
